327e654af52795c6156b114bff4d7573f5dbbb84
imwebson <websonsky+note@gmail.com>
2016-11-19 23:46:04 +0800

    good night !

--- classify.py
+++ classify.py
@@ -9,9 +9,11 @@ param={
     'min_child_weight':1,
     'save_period':0,
     'booster':'gbtree',
-    'objective':'reg:linear'
+    'objective':'reg:linear',
+    #'objective':'count:poisson'
 }
-num_round=2
+num_round=10
+learning_rates=[1.0,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1]
 watchlist=[(dvalid,'eval'),(dtrain,'train')]
-bst=xgb.train(param,dtrain,num_round,watchlist)
+bst=xgb.train(param,dtrain,num_round,watchlist,learning_rates=learning_rates)
 preds=bst.predict(dvalid)
