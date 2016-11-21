027f9dad751c1c4e015719227bb2a1a97a3315d1
htb <huangtb93@126.com>
2016-11-21 21:51:10 +0800

    初步尝试了泊松回归和线性回归的组合实现

--- classify.py
+++ classify.py
@@ -1,19 +1,53 @@
 import xgboost as xgb
+import utils
 
-dtrain=xgb.DMatrix("train.txt")
-dvalid=xgb.DMatrix("valid.txt")
-param={
-    'max_depth':3,
-    'eta':1.0,
-    'gamma':1.0,
-    'min_child_weight':1,
-    'save_period':0,
-    'booster':'gbtree',
-    'objective':'reg:linear',
-    #'objective':'count:poisson'
-}
-num_round=10
-learning_rates=[1.0,0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1]
-watchlist=[(dvalid,'eval'),(dtrain,'train')]
-bst=xgb.train(param,dtrain,num_round,watchlist,learning_rates=learning_rates)
-preds=bst.predict(dvalid)
+
+def classify(maxdepth=3):
+    dtrain = xgb.DMatrix("train.txt")
+    dvalid = xgb.DMatrix("valid.txt")
+    param = {
+        'max_depth': maxdepth,
+        'eta': 1,
+        'gamma': 1,
+        'min_child_weight': 1,
+        'save_period': 0,
+        'booster': 'gbtree',
+        'objective': 'reg:linear',
+    }
+
+    num_round = 15
+    learning_rates = [1.5, 1.4, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.2, 0.2, 0.1, 0.1, 0.1, 0.1]
+    watchlist = [(dvalid, 'eval'), (dtrain, 'train')]
+    bst_o = xgb.train(param, dtrain, num_round, watchlist, learning_rates=learning_rates)
+
+    param = {
+        'max_depth': maxdepth,
+        'eta': 1,
+        'gamma': 1,
+        'min_child_weight': 1,
+        'save_period': 0,
+        'booster': 'gbtree',
+         'objective':'count:poisson'
+    }
+
+    num_round = 15
+    learning_rates = [1.5, 1.4, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+    watchlist = [(dvalid, 'eval'), (dtrain, 'train')]
+    bst_p = xgb.train(param, dtrain, num_round, watchlist, learning_rates=learning_rates)
+
+    preds_vo = bst_o.predict(dvalid)
+    preds_vp = bst_p.predict(dvalid)
+    utils.testPred(preds_vo, "valid.txt")
+    utils.testPred(preds_vp, "valid.txt")
+    utils.testPred((preds_vo + preds_vp) / 2, "valid.txt")
+
+    preds_to = bst_o.predict(dtrain)
+    preds_tp = bst_p.predict(dtrain)
+    utils.testPred(preds_to, "train.txt")
+    utils.testPred(preds_tp, "train.txt")
+    utils.testPred((preds_to + preds_tp) / 2, "train.txt")
+
+
+
+if __name__ == "__main__":
+    classify()
